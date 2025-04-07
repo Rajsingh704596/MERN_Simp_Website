48ba9ca9d353593e0acf854338978cb956315ab3
Raj <rajsingh704596@gmail.com>
2025-04-07 21:37:22 +0530

    error fix schema model and Storing user registered data in DB

--- Backend/controllers/auth-controller.js
+++ Backend/controllers/auth-controller.js
@@ -1,5 +1,7 @@
 // Controller- here handling the application logic , Controllers are typically used to process incoming requests(from routers), interact with models (data sources), and send response back to clients. [MVC pattern]
 
+const User = require("../models/user-model");
+
 //Home logic
 
 const home = async (req, res) => {
@@ -14,8 +16,20 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).json({ message: req.body });
+    // console.log(req.body);
+    const { username, email, phone, password } = req.body; // get registration data
+
+    // check email exist in db
+    const userExist = await User.findOne({ email: email }); // here email pass and check it's exist or not
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exist" });
+    }
+
+    //else register user data in db collection in document form
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(200).json({ message: userCreated });
     // res.status(200).send("welcome to registration page using Controllers");
   } catch (error) {
     res.status(400).json({ msg: "internal server error" });
--- Backend/models/user-model.js
+++ Backend/models/user-model.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 //schema create
 const userSchema = new mongoose.Schema({
-  username: { type: string, require: true },
-  email: { type: string, require: true },
-  phone: { type: string, require: true },
-  password: { type: string, require: true },
+  username: { type: String, require: true },
+  email: { type: String, require: true },
+  phone: { type: String, require: true },
+  password: { type: String, require: true },
   isAdmin: { type: Boolean, default: false },
 });
 
 // define the model/collection name
-const User = new mongoose.model("User"); // so now, In mongodb collection name created users
+const User = new mongoose.model("User", userSchema); // so now, In mongodb collection name created users
 
 module.exports = User;
