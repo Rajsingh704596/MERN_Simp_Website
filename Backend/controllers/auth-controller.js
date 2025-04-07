2013ef8b700ee43e6dfe7bddcd0d053429101535
Raj <rajsingh704596@gmail.com>
2025-04-07 23:21:30 +0530

    Secure user password to hash password using Bcrypt.js

--- Backend/controllers/auth-controller.js
+++ Backend/controllers/auth-controller.js
@@ -1,6 +1,7 @@
 // Controller- here handling the application logic , Controllers are typically used to process incoming requests(from routers), interact with models (data sources), and send response back to clients. [MVC pattern]
 
 const User = require("../models/user-model");
+// const bcrypt = require("bcryptjs");
 
 //Home logic
 
@@ -26,11 +27,20 @@ const register = async (req, res) => {
       return res.status(400).json({ msg: "email already exist" });
     }
 
-    //else register user data in db collection in document form
-    const userCreated = await User.create({ username, email, phone, password });
+    //^ hash password using bcryptjs (npm package)        - we have 2 option either we use here or use schema where mongoose give pre-method
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound);
 
-    res.status(200).json({ message: userCreated });
-    // res.status(200).send("welcome to registration page using Controllers");
+    //else register user data in db collection in document form
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      // password: hash_password,
+      password,
+    });
+
+    res.status(201).json({ message: userCreated });
   } catch (error) {
     res.status(400).json({ msg: "internal server error" });
   }
--- Backend/models/user-model.js
+++ Backend/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 //schema create
 const userSchema = new mongoose.Schema({
@@ -7,6 +8,27 @@ const userSchema = new mongoose.Schema({
   phone: { type: String, require: true },
   password: { type: String, require: true },
   isAdmin: { type: Boolean, default: false },
+});
+
+//^ secure the password (hash)with the bcryptjs using pre method which gives by mongoose
+userSchema.pre("save", async function () {
+  // it's work like middleware where it run this fun before create/save document in collection inside db
+  // console.log("pre method", this); // here this keyword gives current register details
+
+  const user = this;
+
+  // if password not modified(means already secure by bcrypt) so next step go to auth-controller ( that is store in db)
+  if (!user.isModified("password")) {
+    next();
+  }
+  // else password secure with hash pass
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password; // here actual password change with hash_password          // then next go to auth-controller
+  } catch (error) {
+    next(error);
+  }
 });
 
 // define the model/collection name
