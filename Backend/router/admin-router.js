67c00c29cbd44339335bc284a212c2b07a36aff0
Raj <rajsingh704596@gmail.com>
2025-04-23 16:46:52 +0530

    Backend:Secure our Admin Route with JWT verification(user must login) by using auth-middleware then check Admin Authentication (user is admin(means isAdmin:true)) in admin-middleware by req.user which get after jwt verify , then next admin route show all user data (same with contacts)

--- Backend/router/admin-router.js
+++ Backend/router/admin-router.js
@@ -4,8 +4,10 @@ const {
   getAllUsers,
   getAllContacts,
 } = require("../controllers/admin-controller");
+const authMiddleware = require("../middleware/auth-middleware");
+const adminMiddleware = require("../middleware/admin-middleware");
 
-router.route("/users").get(getAllUsers);
-router.route("/contacts").get(getAllContacts);
+router.route("/users").get(authMiddleware, adminMiddleware, getAllUsers); //when hit /api/admin/users url first authMiddleware check for admin panel(dashboard) user is login or not (if login means token pass from client to backend then Jwt token verify)  // second adminMiddleware here check after jwt token user data get so it's admin id or not (isAdmin is false or true) if it's true then further next step ,  //then at last all user data show in admin panel from db
+router.route("/contacts").get(authMiddleware, adminMiddleware, getAllContacts); //same with contacts
 
 module.exports = router;
